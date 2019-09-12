fa1ecc43b31e0123f13a4994087b441124e4c573
whizper <ellioth.love@gmail.com>
2019-09-12 10:56:31 +0200

    odfk

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-        <div class="container">
-            <h1>Bruh!</h1>
 <?php
 include 'include/dbinfo.php';
-
 try {
     $dbh = new PDO(
         'mysql:host=localhost;dbname=' . $database . '',
@@ -22,22 +10,10 @@ try {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
-// select * from tweet
-$sth = $dbh->prepar('SELECT * FROM tweet
+$sth = $dbh->prepare('SELECT * FROM tweet
             JOIN users
             ON tweet.user_id = users.id');
 $sth->execute();
 $result = $sth->fetchAll(PDO::FETCH_ASSOC);
-//echo "<pre>" . print_r($result,1) . "</pre>"
- foreach ($result as $row) {
-        //echo "<pre>" . print_r($row,1) . "</pre>";
-        echo "<div>";
-        echo "<h2>" . $row['body'] . "</h2>";
-        echo "<p>" . $row['name'] . "</p>";
-        echo "</div>";
-    
-}
-?>
-        </div>
-</body>
-</html>
+include 'views/index_layout.php';
+?>  
